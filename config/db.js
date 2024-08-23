600242c4c6650fd1a34441a22d69fa1408a7364a
Ajay Wankhade <156012556+ajju90@users.noreply.github.com>
2024-08-24 00:27:38 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: 'Avinash@83083',
+    host: 'database-1.c5gw0is62vmi.ap-south-1.rds.amazonaws.com',
+    user: 'admin',
+    password: '123456789',
     database: 'testdb',
 };
 
